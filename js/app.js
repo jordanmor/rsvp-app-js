6a7308904e22c5dd7c61c0107f2ab6d3de6517c5
Jordan Mor <jordanmor@gmail.com>
2018-06-24 16:39:07 -0400

    Add background image slideshow functionality

---
+++ js/app.js
@@ -0,0 +1,7 @@
+easy_background("header", 
+    {
+        slide: ["../images/outside-venue.jpg", "../images/restaurant.jpg", "../images/concert.jpg",
+                "../images/wedding.jpg", "../images/picnic.jpg"],
+        delay: [15000,15000,15000,15000,15000]
+    }
+);
