b9ae337cbfc4db947a9eb00b49c8621a173fca4d
Jordan Mor <jordanmor@gmail.com>
2018-06-27 18:40:40 -0400

    Refactor submit event handler

--- js/app.js
+++ js/app.js
@@ -10,10 +10,7 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const text = input.value;
-    input.value = '';
+function createLI(text) {
     const li = document.createElement('li');
     li.textContent = text;
     const label = document.createElement('label');
@@ -25,9 +22,18 @@ form.addEventListener('submit', e => {
 
     const button = document.createElement('button');
     button.textContent = 'remove';
-    
+
     li.appendChild(label);
     li.appendChild(button);
+    return li;
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const li = createLI(text);
+
     ul.appendChild(li);
 });
 
