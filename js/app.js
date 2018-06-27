211dd6b1d429b6364c070caffd287336182db86c
Jordan Mor <jordanmor@gmail.com>
2018-06-27 18:09:02 -0400

    Add event listener to handle checkboxes on list items

--- js/app.js
+++ js/app.js
@@ -8,13 +8,28 @@ easy_background("header",
 
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     const text = input.value;
     input.value = '';
-    const ul = document.getElementById('invitedList');
     const li = document.createElement('li');
     li.textContent = text;
+    const label = document.createElement('label');
+    label.textContent = 'Confirmed';
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    label.appendChild(checkbox);
+    li.appendChild(label);
     ul.appendChild(li);
+});
+
+ul.addEventListener('change', e => {
+    const checkbox = e.target;
+    const checked = checkbox.checked;
+    const li = checkbox.parentElement.parentElement;
+    
+    checked ? li.className = 'responded' : li.className = '';
+
 });
