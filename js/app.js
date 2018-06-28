501c83f27a2fa569100317ca888a97ed5db496c2
Jordan Mor <jordanmor@gmail.com>
2018-06-28 07:44:41 -0400

    Complete edit button functionality

--- js/app.js
+++ js/app.js
@@ -12,7 +12,10 @@ const ul = document.getElementById('invitedList');
 
 function createLI(text) {
     const li = document.createElement('li');
-    li.textContent = text;
+    const span = document.createElement('span');
+    span.textContent = text;
+    li.appendChild(span);
+
     const label = document.createElement('label');
     label.textContent = 'Confirmed';
 
@@ -55,11 +58,24 @@ ul.addEventListener('click', e => {
         const button = e.target;
         const li = button.parentElement;
         const ul = li.parentElement;
-        
+
         if(button.textContent === 'remove') {
             ul.removeChild(li);
         } else if(button.textContent === 'edit') {
-            console.log('edit');
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = 'save';
+        } else if(button.textContent === 'save') {
+            const input = li.firstElementChild;
+            const span = document.createElement('span');
+            span.textContent = input.value;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'edit';
         }
     }
 });
